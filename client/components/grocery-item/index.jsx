d8f2096fefdc6f3c9f0ce252ac8d150d36bd99d7
Guy Lemon <guylemon.home@gmail.com>
2019-01-01 18:55:04 -0600

    feat(client): adds pwa manifest to html template, schema.org metadata, viewport and theme-color meta tags.

--- client/components/grocery-item/index.jsx
+++ client/components/grocery-item/index.jsx
@@ -5,6 +5,24 @@ import { endpoint as API_ENDPOINT } from '../../utils/api';
 
 import './styles.scss';
 
+/**
+ * getSchemaData
+ *
+ * @description
+ * Formats schema.org style product data for each grocery item.
+ *
+ * @param {object} item
+ * @returns {object} schemaData
+ */
+function getSchemaData(item) {
+  return JSON.stringify({
+    '@context': 'http://schema.org',
+    '@type': 'Product',
+    image: item.imageUrl,
+    name: item.name,
+  });
+}
+
 function formatPrice(rawPrice) {
   return `$${rawPrice.toFixed(2)}`;
 }
@@ -15,18 +33,24 @@ const GroceryItem = ({ item, cartStore }) => {
 
   let unit = item.unit;
   return (
-    <li className='GroceryItem mui-panel'>
-      <img className='item-image' src={itemUrl} alt={item.name} />
-      <h4 className='item-name'>{item.name}</h4>
+    <li className="GroceryItem mui-panel">
+      <img className="item-image" src={itemUrl} alt={item.name} />
+      <h4 className="item-name">{item.name}</h4>
       <span className="item-price bottom-tile bottom-tile--right">
         {price}
-        {unit ? <span className='item-unit'>{unit}</span> : ''}
+        {unit ? <span className="item-unit">{unit}</span> : ''}
       </span>
-      <button onClick={() => {
-        cartStore.addItemToCart(item)
-      }} className="add-item-to-cart bottom-tile bottom-tile--left mui-btn mui-btn--accent" >+</button>
+      <button
+        onClick={() => {
+          cartStore.addItemToCart(item);
+        }}
+        className="add-item-to-cart bottom-tile bottom-tile--left mui-btn mui-btn--accent"
+      >
+        +
+      </button>
+      <script>{getSchemaData(item)}</script>
     </li>
-  )
+  );
 };
 
 export default GroceryItem;
--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import VAPID from './../private/vapid.json';
+import './web-manifest/web-app-manifest.json'
 
 import 'file-loader?name=./img/launcher-icon-1x.png!./img/launcher-icon-1x.png';
 import 'file-loader?name=./img/launcher-icon-2x.png!./img/launcher-icon-2x.png';
@@ -17,6 +18,7 @@ import 'file-loader?name=./apple-touch-icon-120x120.png!./img/apple-touch-icon-1
 import 'file-loader?name=./apple-touch-icon-144x144.png!./img/apple-touch-icon-144x144.png';
 import 'file-loader?name=./apple-touch-icon-152x152.png!./img/apple-touch-icon-152x152.png';
 import 'file-loader?name=./apple-touch-icon-180x180.png!./img/apple-touch-icon-180x180.png';
+
 
 ReactDOM.render((<App />), document.getElementById('root'));
 
