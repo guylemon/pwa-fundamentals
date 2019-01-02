a58a8a54189d8e21cf02f937c4af4d3e6344f675
Guy Lemon <guylemon.home@gmail.com>
2019-01-01 18:54:20 -0600

    feat(webpack): adds rule for web-app-manifest

--- webpack/module.config.js
+++ webpack/module.config.js
@@ -1,8 +1,32 @@
 /* eslint-env node */
 
-const extractSass = require('./extract-sass');
+/**
+ * webAppManifest
+ *
+ * @description
+ * When we find the web-app-manifest.json file, we need that to be output to the
+ * root of dist.  Also, we need to preserve the name so that we can import it
+ * directly from the template ejs file.
+ *
+ * NOTE: This is a workaround for an issue where using the inline syntax for json
+ * files is not working correctly.
+ *
+ * @see https://github.com/webpack-contrib/file-loader
+ */
+const webAppManifest = {
+  // Because of a breaking change in webpack version 4, we need to specify a
+  // type of 'javascript/auto' to prevent outputting invalid json.
+  // @see https://github.com/webpack/webpack/blob/master/lib/WebpackOptionsDefaulter.js#L62
+  type: 'javascript/auto',
+  test: /web-app-manifest\.json$/,
+  loader: 'file-loader',
+  options: {
+    // preserve the file name and extension.
+    name: '[name].[ext]'
+  }
+}
 
-module.exports = function() {
+module.exports = function () {
   return {
     rules: [
       {
@@ -32,7 +56,8 @@ module.exports = function() {
             }
           }
         ]
-      }
+      },
+      webAppManifest
     ]
   };
 };
